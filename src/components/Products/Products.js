f28bf3bb27e0fab8d8f32e54c335a22a62b11ba6
ZuhiNaz1 <zuhinaz1518@gmail.com>
2023-04-30 22:11:18 +0100

    project commit

---
+++ src/components/Products/Products.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Products = () => {
+    return (
+        <div>
+            
+            <h3>This is products page.</h3>
+        </div>
+    );
+};
+
+export default Products;
